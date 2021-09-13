78ffce43fc033afcd2311f6c0853ce118f98c429
Dylan Kok <tenkotofu@gmail.com>
2021-09-14 01:19:33 +0800

    Added shutdown feature

--- app/routes/index.js
+++ app/routes/index.js
@@ -5,4 +5,10 @@ router.get('/', async (req, res) => {
     return res.render('index')
 })
 
+router.post('/shutdown', (req, res) => {
+    res.render('shutdown')
+    console.log(chalk.greenBright('Metronami has shut down. It is now safe to close this window.'))
+    return process.exit()
+})
+
 module.exports = router
